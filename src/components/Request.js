f7b996901054a3229ad5152c1e1cfb9019b49985
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2018-11-07 23:17:45 +0100

    fixed some bug

--- src/components/Request.js
+++ src/components/Request.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { startAddLike, startRemoveLike } from '../actions/like';
 import { startAddDislike, startRemoveDislike } from '../actions/dislike';
 import { startDeleteRequest } from '../actions/requests';
-import { confirmAlert } from 'react-confirm-alert'; 
-import 'react-confirm-alert/src/react-confirm-alert.css' 
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css'
 
 export class Request extends React.Component {
   constructor() {
@@ -18,57 +18,52 @@ export class Request extends React.Component {
       plusSymbol: true
     };
   }
-  componentWillReceiveProps(nextProps){
-    this.setState(() => ({ numberOfLikes: nextProps.likes.length }));
-    this.setState(() => ({ numberOfDislikes: nextProps.dislikes.length }));
-    this.setState(() => ({ score: nextProps.likes.length-nextProps.dislikes.length }));
-
-
-    //SETTING PLUS 
-    if((nextProps.likes.length-nextProps.dislikes.length)>0){
-      this.setState(() => ({ plusSymbol:true}));
-    } else if (nextProps.likes.length-nextProps.dislikes.length<=0){
-      this.setState(() => ({ plusSymbol:false}));
-    }
-    
-    
-    
-  }
-
 
   componentDidMount() {
-    
+    //SETTING INITIAL AMOUNTS OF LIKES, DISLIKES AND SCORE
     this.setState(() => ({ numberOfLikes: this.props.likes.length }));
     this.setState(() => ({ numberOfDislikes: this.props.dislikes.length }));
-    this.setState((prevState) => ({ score: prevState.numberOfLikes-prevState.numberOfDislikes }));
-    if(this.state.score>0){
-      this.setState(() => ({ plusSymbol:true}));
-    } else if (this.state.score===0){
-      this.setState(() => ({ plusSymbol:false}));
+    this.setState((prevState) => ({ score: prevState.numberOfLikes - prevState.numberOfDislikes }));
+
+    //SETTING INITIAL PLUS SYMBOLS
+    if (this.props.likes.length-this.props.dislikes.length > 0) {
+      this.setState(() => ({ plusSymbol: true }));
+    } else if (this.state.score <= 0) {
+      this.setState(() => ({ plusSymbol: false }));
     }
-    //HVIS JEG ALLEREDE HAR LIKED, SET STATE.LIKE = TRUE + 1
+
+    //IF ALREADY LIKED, SET STATE.LIKE = TRUE
     for (const key in this.props.likes) {
       if (this.props.likes.hasOwnProperty(key)) {
         var value = this.props.likes[key];
         if (Object.values(value)[0] === this.props.uid) {
           this.setState(() => ({ like: true }))
-        //  this.setState((prevState) => {return {numberOfLikes: prevState.numberOfLikes + 1};});
-          
         }
       }
     }
-    //HVIS JEG ALLEREDE HAR DISLIKED, SET STATE.DISLIKE = TRUE + 1
+    //IF ALREADY DISLIKED, SET STATE.DISLIKE = TRUE
     for (const key in this.props.dislikes) {
       if (this.props.dislikes.hasOwnProperty(key)) {
         var value = this.props.dislikes[key];
         if (Object.values(value)[0] === this.props.uid) {
           this.setState(() => ({ dislike: true }))
-          //this.setState((prevState) => {return {numberOfDislikes: prevState.numberOfDislikes + 1};});
-          
         }
       }
     }
-    
+  }
+
+  componentWillReceiveProps(nextProps) {
+    //LISTENING AND SETTING STATE WITH THE NEW LIKE AND DISLIKE VALUES
+    this.setState(() => ({ numberOfLikes: nextProps.likes.length }));
+    this.setState(() => ({ numberOfDislikes: nextProps.dislikes.length }));
+    this.setState(() => ({ score: nextProps.likes.length - nextProps.dislikes.length }));
+
+    //LISTENING AND SETTING PLUS ON NEW LIKE AND DISLIKES
+    if ((nextProps.likes.length - nextProps.dislikes.length) > 0) {
+      this.setState(() => ({ plusSymbol: true }));
+    } else if (nextProps.likes.length - nextProps.dislikes.length <= 0) {
+      this.setState(() => ({ plusSymbol: false }));
+    }
   }
 
   likeHandler = () => {
@@ -77,22 +72,20 @@ export class Request extends React.Component {
         uid: this.props.uid,
         name: this.props.name
       }
-
       this.setState(() => ({ like: true }), () => {
         this.props.onLike(this.props.id, likeObject);
       });
-    if (this.state.dislike === true) {
+      if (this.state.dislike === true) {
         this.setState(() => ({ dislike: false }), () => {
           this.props.onRemoveDislike(this.props.id, this.findReactionId(this.props.dislikes));
         });
       }
-  } else if (this.state.like === true) {
+    } else if (this.state.like === true) {
       this.setState(() => ({ like: false }), () => {
         this.props.onRemoveLike(this.props.id, this.findReactionId(this.props.likes));
       });
     }
   }
-
 
   dislikeHandler = () => {
     if (this.state.dislike !== true) {
@@ -100,7 +93,6 @@ export class Request extends React.Component {
         uid: this.props.uid,
         name: this.props.name
       }
-
       this.setState(() => ({ dislike: true }), () => {
         this.props.onDislike(this.props.id, dislikeObject);
       });
@@ -126,14 +118,9 @@ export class Request extends React.Component {
     return foundReactionId;
   }
 
-  delete = () => {
+  deleteRequest = () => {
     this.props.onDeleteRequest(this.props.id);
   }
-
-  getState = () => {
-    console.log(this.state)
-  }
-
 
   submit = () => {
     confirmAlert({
@@ -142,7 +129,7 @@ export class Request extends React.Component {
       buttons: [
         {
           label: 'Yes',
-          onClick: () => this.delete()
+          onClick: () => this.deleteRequest()
         },
         {
           label: 'No',
@@ -150,8 +137,6 @@ export class Request extends React.Component {
       ]
     })
   };
- 
-  
 
   render() {
     return (
@@ -161,10 +146,9 @@ export class Request extends React.Component {
             <h3 className="song-title">{this.props.title}</h3>
             <p className="artist-name">{this.props.artist}</p>
           </td>
-          <td> 
-              <h1>{this.state.plusSymbol?'+':''}{this.state.score}</h1>           
-        </td>
-    
+          <td>
+            <h1>{this.state.plusSymbol ? '+' : ''}{this.state.score}</h1>
+          </td>
           <td className="vote-column">
             <p>{this.state.numberOfLikes}</p>
             <button className="vote-btn like" onClick={this.likeHandler}>
@@ -172,16 +156,16 @@ export class Request extends React.Component {
             </button>
           </td>
           <td className="vote-column">
-          <p>{this.state.numberOfDislikes}</p>
-          <button className="vote-btn dislike" onClick={this.dislikeHandler}>
-            <i className="material-icons" style={this.state.dislike ? { color: 'red' } : null}>thumb_down</i>
-          </button>
-        </td>
+            <p>{this.state.numberOfDislikes}</p>
+            <button className="vote-btn dislike" onClick={this.dislikeHandler}>
+              <i className="material-icons" style={this.state.dislike ? { color: 'red' } : null}>thumb_down</i>
+            </button>
+          </td>
           <td>
-          {this.props.createdBy===this.props.uid?<button onClick={this.submit}>X</button>:null}
+            {this.props.createdBy === this.props.uid ? <button onClick={this.submit}>X</button> : null}
+          </td>
+          <td>
         </td>
-        <td>
-      </td>
         </tr>
       </React.Fragment>
     )
