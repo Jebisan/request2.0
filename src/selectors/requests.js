b79cb740409d337f385242c5416205eef676fc6c
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2018-11-09 01:31:47 +0100

    sorting optimized

--- src/selectors/requests.js
+++ src/selectors/requests.js
@@ -2,7 +2,7 @@
 
 export default (requests) => {
     return requests.sort((a, b) => {
-        return (a.likes.length-a.dislikes.length) < (b.likes.length-b.dislikes.length) ? 1 : -1;
+        return (a.likes.length-a.dislikes.length) <= (b.likes.length-b.dislikes.length) ? 1 : -1;
     });
   };
   
